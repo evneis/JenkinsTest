e21bd15c57137ce3e3333ee75bd1494263ec057a
Evan Neiswender <esn002@gmail.com>
2021-04-30 13:14:19 -0400

    first commit

---
+++ src/main/java/gittest/jenkins/App.java
@@ -0,0 +1,14 @@
+package gittest.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Welcome to the test application!");
+    }
+}
