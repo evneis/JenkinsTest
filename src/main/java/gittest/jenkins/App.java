774e9c7eaa5d5ced175009348384ab10870b5b67
Evan Neiswender <esn002@gmail.com>
2021-05-05 13:29:56 -0400

    added a new line

--- src/main/java/gittest/jenkins/App.java
+++ src/main/java/gittest/jenkins/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("Welcome to the test application!");
         System.out.println("Maybe one day you can interact with the program here.");
+        System.out.println("This is a new line added and pushed to github");
     }
 }
